1949d466ac83ead98d174aebbc79ea357c5a91d6
Daniel T. <yepyyai@gmail.com>
2026-05-05 19:52:48 -0400

    fix: add libclang for native wheel builds

--- docker/run/fs/ins/pre_install.sh
+++ docker/run/fs/ins/pre_install.sh
@@ -3,7 +3,7 @@ set -e
 
 # update apt
 apt-get update
-apt-get install -y --no-install-recommends cmake
+apt-get install -y --no-install-recommends cmake libclang-dev
 
 # fix permissions for cron files if any
 if [ -f /etc/cron.d/* ]; then
